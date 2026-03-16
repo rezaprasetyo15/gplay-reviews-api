d453c3f6c11e7c523b5de4a11187169f085212f6
rezaprasetyo15 <reza.prasetyo@flip.id>
2026-03-16 15:38:39 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+{
+  "name": "gplay-reviews-api",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "google-play-scraper": "^9.1.0"
+  }
+}
